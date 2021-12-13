65db4d1077e3eddd9ce3d390ff6f8493126318b4
Kellyson Dias <kellysondias@outlook.com>
2021-12-13 15:54:34 -0300

    Changing the 'var' variables to 'const' ones

--- desafio-3/index.js
+++ desafio-3/index.js
@@ -1,6 +1,6 @@
-var valorDaHospedagem = 50 + ",00"
+const valorDaHospedagem = 50 + ",00"
 
-var aventureiro = confirm("Seja bem-vinde aventureire! Gostaria de pagar R$" + valorDaHospedagem + " para passar a noite na nossa hospedagem?")
+const aventureiro = confirm("Seja bem-vinde aventureire! Gostaria de pagar R$" + valorDaHospedagem + " para passar a noite na nossa hospedagem?")
 
 if (aventureiro) {
     alert("Ótimo! Nós temos as melhores camas de toda a região!")
